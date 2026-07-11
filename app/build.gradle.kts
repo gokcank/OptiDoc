d4e5de8badd050bc1e04137b4b9d92abff252dc1
Hasan Gökcan KAHRAMAN <hgokcankahraman@gmail.com>
2026-07-11 03:24:47 +0300

    chore: bump version to 1.1.0 and update gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.gokcank.optidoc"
         minSdk        = 26
         targetSdk     = 36
-        versionCode   = 1
-        versionName   = "1.0.0"
+        versionCode   = 2
+        versionName   = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
